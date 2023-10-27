30ee3fd6d99c525775c6e5113887f85f720336e2
ashen-disanayaka <disanayakaaashen@gmail.com>
2023-10-27 15:16:09 +0530

    change code

---
+++ q&a.js
@@ -0,0 +1,100 @@
+ 
+    document.addEventListener('DOMContentLoaded', function () {
+        const button = document.getElementById('collapseButton1');
+        const content = document.getElementById('collapsibleContent1');
+
+        button.addEventListener('click', function () {
+            if (content.classList.contains('show')) {
+                content.classList.remove('show');
+            } else {
+                content.classList.add('show');
+            }
+        });
+    });
+
+    document.addEventListener('DOMContentLoaded', function () {
+        const button = document.getElementById('collapseButton2');
+        const content = document.getElementById('collapsibleContent2');
+
+        button.addEventListener('click', function () {
+            if (content.classList.contains('show')) {
+                content.classList.remove('show');
+            } else {
+                content.classList.add('show');
+            }
+        });
+    });
+    
+    document.addEventListener('DOMContentLoaded', function () {
+        const button = document.getElementById('collapseButton3');
+        const content = document.getElementById('collapsibleContent3');
+
+        button.addEventListener('click', function () {
+            if (content.classList.contains('show')) {
+                content.classList.remove('show');
+            } else {
+                content.classList.add('show');
+            }
+        });
+    });
+    
+    document.addEventListener('DOMContentLoaded', function () {
+        const button = document.getElementById('collapseButton4');
+        const content = document.getElementById('collapsibleContent4');
+
+        button.addEventListener('click', function () {
+            if (content.classList.contains('show')) {
+                content.classList.remove('show');
+            } else {
+                content.classList.add('show');
+            }
+        });
+    });
+
+    
+    document.addEventListener('DOMContentLoaded', function () {
+        const button = document.getElementById('collapseButton5');
+        const content = document.getElementById('collapsibleContent5');
+
+        button.addEventListener('click', function () {
+            if (content.classList.contains('show')) {
+                content.classList.remove('show');
+            } else {
+                content.classList.add('show');
+            }
+        });
+    });
+
+    
+    document.addEventListener('DOMContentLoaded', function () {
+        const button = document.getElementById('collapseButton6');
+        const content = document.getElementById('collapsibleContent6');
+
+        button.addEventListener('click', function () {
+            if (content.classList.contains('show')) {
+                content.classList.remove('show');
+            } else {
+                content.classList.add('show');
+            }
+        });
+    });
+
+    
+    document.addEventListener('DOMContentLoaded', function () {
+        const button = document.getElementById('collapseButton7');
+        const content = document.getElementById('collapsibleContent7');
+
+        button.addEventListener('click', function () {
+            if (content.classList.contains('show')) {
+                content.classList.remove('show');
+            } else {
+                content.classList.add('show');
+            }
+        });
+    });
+
+
+//----------------- animation-------------------------------------------
+
+
+ 
